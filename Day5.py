5088700ff686f817e408849e882aab9d56e11769
suprit-kumar <supritk16@gmail.com>
2022-12-09 07:14:23 +0530

    Python built-in and data types theory added

---
+++ Day5.py
@@ -0,0 +1,6 @@
+# print is a python built-in function
+# Python scripts executes line by line
+# \n -> Is an Escape sequence character
+# Written code or Messages can be ignored through compiler
+
+print("Hello world")
---
+++ Day6.py
@@ -0,0 +1,20 @@
+# Variables are like containers in python, Which stores the data.
+# These containers store data in computer memory
+# Datatypes are the different forms of the data.
+# Datatype specifies the type of value of a variable holds.
+
+a = 1232313 # Integer type
+b = "python" # String type
+c = True # Boolean
+
+# These are the built-in data types in python and There can many more data types.
+# Different data types required in programming to perform different operations
+# For concatenate datatype need to be same of both variables
+
+# Mutable (Comes from Mutation) - Changeable
+# Immutable - Non-changeable
+
+# Everything is object in python
+
+# Explore More ........................
+
