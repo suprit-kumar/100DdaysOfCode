bb87b3b2c42e8da65e88cc18bd05d88e17d192c0
suprit-kumar <supritk16@gmail.com>
2023-01-03 19:08:50 +0530

    Finally clasuse

---
+++ Day_36.py
@@ -0,0 +1,27 @@
+# Finally Clause
+
+"""
+The finally code block is also a part of exception handling.
+When we handle exception using the try and except block,
+we can include a finally block at the end. The finally block is always executed,
+so it is generally used for doing the concluding tasks like closing file resources or
+closing database connection or may be ending the program execution with a delightful message.
+"""
+
+def func1():
+  try:
+    l = [1, 5, 6, 7]
+    i = int(input("Enter the index: "))
+    print(l[i])
+    return 1
+  except:
+    print("Some error occurred")
+    return 500
+
+  finally:
+    print("I am always executed")
+  # print("I am always executed")
+
+
+x = func1()
+print(x)
