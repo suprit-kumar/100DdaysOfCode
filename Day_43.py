bf9f4d495a8a84bed5a0277cab2e642e2832d6b4
suprit-kumar <supritk16@gmail.com>
2023-01-12 19:59:28 +0530

    os module in python

---
+++ Day_43.py
@@ -0,0 +1,29 @@
+"""
+os Module in Python
+-------------------
+The os module in Python is a built-in library that provides functions for interacting with the operating system.
+It allows you to perform a wide variety of tasks, such as reading and writing files,
+interacting with the file system, and running system commands.
+
+"""
+
+
+import os
+if(not os.path.exists("data")):
+    os.mknod(f"{os.getcwd()}/data.py")
+
+
+
+"""
+Interacting with the file system
+--------------------------------
+The os module also provides functions for interacting with the file system. 
+For example, you can use the os.listdir function to get a list of the files in a directory:
+"""
+
+import os
+
+# Get a list of the files in the current directory
+files = os.listdir(".")
+print(files)
+
