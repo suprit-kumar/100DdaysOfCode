57a9b90ef6566704a1583d721ac96b66de0979bf
suprit-kumar <supritk16@gmail.com>
2023-01-29 19:19:38 +0530

    snake game example2

--- Day_51.py
+++ Day_51.py
@@ -19,6 +19,7 @@ Use proper functions to check for win.
 
 import random
 
+# Example-1
 
 def snake_water_gun():
     userInput = int(input("Enter any number between 0 to 2: "))
@@ -40,4 +41,38 @@ def snake_water_gun():
     if userInput == 2 and randomNum == 1:
         print("It's water. You lost")
 
-snake_water_gun()
+# snake_water_gun()
+
+
+
+# Example-2
+def check(comp, user):
+    if comp == user:
+        return 0
+
+    if (comp == 0 and user == 1):
+        return -1
+
+    if (comp == 1 and user == 2):
+        return -1
+
+    if (comp == 2 and user == 0):
+        return -1
+
+    return 1
+
+
+comp = random.randint(0, 2)
+user = int(input("0 for Snake, 1 for water and 2 for Gun:\n"))
+
+score = check(comp, user)
+
+print("You: ", user)
+print("Computer: ", comp)
+
+if (score == 0):
+    print("Its a draw")
+elif (score == -1):
+    print("You Lose")
+else:
+    print("You Won")
