85f66697f5d2ffa3e03ddc80d771b8fae1e805ff
suprit-kumar <supritk16@gmail.com>
2023-02-25 20:24:44 +0530

    request module

---
+++ Day_80.py
@@ -0,0 +1,34 @@
+"""
+Requests module in python
+-------------------------
+The Python Requests module is an HTTP library that enables developers to send HTTP requests in Python.
+This module enables you to send HTTP requests using Python code and makes it possible to interact with APIs and web services.
+
+"""
+
+import requests
+from bs4 import BeautifulSoup
+url = "https://www.codewithharry.com/blogpost/django-cheatsheet/"
+r = requests.get(url)
+# print(r.text)
+
+
+# soup = BeautifulSoup(r.text, 'html.parser')
+# print(soup.prettify())
+# for heading in soup.find_all("h2"):
+#   print(heading.text)
+
+
+url = "https://jsonplaceholder.typicode.com/posts"
+
+data = {
+    "title": 'harry',
+    "body": 'bhai',
+    "userId": 12,
+  }
+headers =  {
+    'Content-type': 'application/json; charset=UTF-8',
+  }
+response = requests.post(url, headers=headers, json=data)
+
+print(response.text)
