67bbd74a9736b1335cb185db8c639f1f14abd16c
suprit-kumar <supritk16@gmail.com>
2023-03-05 19:44:37 +0530

    multithreading concept

--- Day_87.py
+++ Day_87.py
@@ -42,4 +42,18 @@ def main():
     print(time2 - time1)
 
 
-main()
+def poolingDemo():
+  with ThreadPoolExecutor() as executor:
+    # future1 = executor.submit(func, 3)
+    # future2 = executor.submit(func, 2)
+    # future3 = executor.submit(func, 4)
+    # print(future1.result())
+    # print(future2.result())
+    # print(future3.result())
+    l = [3, 5, 1, 2]
+    results = executor.map(func, l)
+    for result in results:
+      print(result)
+
+
+poolingDemo()
